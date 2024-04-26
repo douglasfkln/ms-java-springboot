83404694cd9489cfd0b86a7a896298de4720b0b4
Douglas Franklin <douglas.silva@livelo.com.br>
2024-04-25 22:56:03 -0300

    Configurado banco de dados e codificado classes de modelo de domínio

---
+++ src/main/java/com/uri/progweb/userdep/entities/User.java
@@ -0,0 +1,54 @@
+package com.uri.progweb.userdep.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id // Diz que esse campo é a chave primária do banco
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // é uma forma de gerar os IDs
+    private Long id;
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public User () {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
---
+++ src/test/java/com/uri/progweb/userdep/UserdepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uri.progweb.userdep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
